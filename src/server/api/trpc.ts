c4b9d60313807f147f5dc6f4a085e8b57d36fd84
Sri Tikkireddy <sri.tikkireddy@databricks.com>
2024-04-26 09:21:12 -0400

    clean up imports

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -6,11 +6,9 @@
  * TL;DR - This is where all the tRPC server stuff is created and plugged in. The pieces you will
  * need to use are documented accordingly near the end.
  */
-import { initTRPC } from "@trpc/server";
+import {initTRPC} from "@trpc/server";
 import superjson from "superjson";
-import { ZodError } from "zod";
-import {DBSQLClient} from "@databricks/sql";
-import {env} from "@/env";
+import {ZodError} from "zod";
 
 /**
  * 1. CONTEXT
